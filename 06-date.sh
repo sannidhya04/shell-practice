412fe48e36f46a9c10c0be844dbe9af203d6efc2
sannidhya04 <arikatla.sannidhya.com>
2026-01-31 21:43:09 +0530

    conversation

--- 06-date.sh
+++ 06-date.sh
@@ -1,13 +1,13 @@
 
 #!/bin/bash
 
-START_TIME = $(date +%s)
+START_TIME=$(date +%s)
 echo "Script executed at $START_TIME"
 
 sleep 10
 
-END_TIME = $(date +%s)
+END_TIME=$(date +%s)
 
-TOTAL_TIME = $((ENDT_IME-START_TIME))
+TOTAL_TIME=$((END_TIME-START_TIME))
 
 echo "Script executed in $TOTAL_TIME"
