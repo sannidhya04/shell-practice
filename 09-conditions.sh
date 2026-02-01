5eefe45cf535ede0039f8a7d37e7c4d057197fbe
sannidhya04 <arikatla.sannidhya.com>
2026-02-01 17:16:55 +0530

    actions

---
+++ 09-conditions.sh
@@ -0,0 +1,25 @@
+
+#!bin/bash
+
+$NUMBER = 20
+
+-gt
+-lt
+-eq
+-ne
+
+if [ $NUMBER -gt 20 ]; then
+    echo "Given number $NUMBER is greater than 20"
+
+elif [ $NUMBER -eq 20 ]; then
+    echo "Given number $NUMBER is equal to 20"
+
+else 
+
+    echo "Given number $NUMBER is less tham 20"
+
+fi
+
+
+
+
