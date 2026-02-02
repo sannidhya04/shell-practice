6a50261cd708f7f7d91fe7fe9023f57f63304ed2
sannidhya04 <arikatla.sannidhya.com>
2026-02-02 21:25:48 +0530

    actions

--- 18-trap.sh
+++ 18-trap.sh
@@ -2,7 +2,9 @@
 
 set -e #ERR
 
-trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERR
+trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERR 
+
+#if any command fails, instead of silently exiting, Bash will print a helpful message showing where and what failed.
 
 echo "Hello World"
 echo "I am learning Shell"
