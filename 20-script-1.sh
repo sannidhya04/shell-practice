1cf2f209dc5feaeaadd2f950ec57f42871e5b572
sannidhya04 <arikatla.sannidhya.com>
2026-02-02 21:35:25 +0530

    actions

---
+++ 20-script-1.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+
+COUNTRY="INDIA"
+
+echo "I'm in: $COUNTRY "
+echo "PID of script-1 is : $$ "
+
+sh 21-script-2.sh
---
+++ 21-script-2.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+
+COUNTRY="INDIA"
+
+echo "I'm in: $COUNTRY "
+echo "PID of $COUNTRY is : $$ "
+
+sh 21-script-2.sh
